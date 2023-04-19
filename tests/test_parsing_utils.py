37e752c2d04ffe30ef3fa9497f42e932eb07455f
Graham Herceg <gherceg@dimagi.com>
2023-04-19 07:50:54 -0700

    add tests for parsing_utils.vsplit

--- tests/test_parsing_utils.py
+++ tests/test_parsing_utils.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from package_metrics.parsing_utils import behind
+from package_metrics.parsing_utils import behind, vsplit
 
 
 class BehindTests(TestCase):
@@ -43,3 +43,22 @@ class BehindTests(TestCase):
     def test_multi_patch_delta(self):
         delta = behind("1.0.5", "1.0.0")
         self.assertEqual(delta, [0, 0, 5])
+
+
+class VsplitTests(TestCase):
+
+    def test_major_minor_patch(self):
+        version = vsplit("3.2.1")
+        self.assertEqual(version, [3, 2, 1])
+
+    def test_major_minor(self):
+        version = vsplit("3.2")
+        self.assertEqual(version, [3, 2, 0])
+
+    def test_major(self):
+        version = vsplit("3")
+        self.assertEqual(version, [3, 0, 0])
+
+    def test_non_semvar(self):
+        version = vsplit("4.3.2.1")
+        self.assertEqual(version, [4, 3, 2])
