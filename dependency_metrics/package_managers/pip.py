d7c85f6473ccf6b0c07a37132ac59b7480ce6fe2
Graham Herceg <gherceg@dimagi.com>
2023-05-23 17:31:13 -0700

    support total count for pip packages

--- dependency_metrics/package_managers/pip.py
+++ dependency_metrics/package_managers/pip.py
@@ -7,7 +7,7 @@ def get_pip_packages():
     """
     Return relevant package version info from pip list output
     """
-    pip_packages = json.loads(Pip.list())
+    pip_packages = json.loads(Pip.list(outdated=True))
     cleaned_package_info_list = []
     for pkg_info in pip_packages:
         del pkg_info['latest_filetype']
@@ -15,11 +15,22 @@ def get_pip_packages():
     return cleaned_package_info_list
 
 
+def get_total_count_for_pip():
+    """
+    Return total number of installed dependencies
+    """
+    packages = json.loads(Pip.list())
+    return len(packages)
+
+
 class Pip:
 
     @staticmethod
-    def list():
+    def list(outdated=False):
         """
-        Equivalent to ``pip list --format json --outdated``
+        Equivalent to ``pip list --format json [--outdated]``
         """
-        return sh.pip("list", "--format", "json", "--outdated")
+        args = ["list", "--format", "json"]
+        if outdated:
+            args.append("--outdated")
+        return sh.pip(*args)
--- tests/package_managers/test_pip.py
+++ tests/package_managers/test_pip.py
@@ -1,6 +1,7 @@
 from unittest import TestCase, mock
 
-from dependency_metrics.package_managers.pip import get_pip_packages
+from dependency_metrics.package_managers.pip import (get_pip_packages,
+                                                     get_total_count_for_pip)
 
 
 @mock.patch('dependency_metrics.package_managers.pip.Pip')
@@ -14,3 +15,15 @@ class GetPipPackagesTests(TestCase):
         result = get_pip_packages()
         self.assertEqual(result, [
             {"name": "test", "version": "1.0", "latest_version": "5.0"}])
+
+
+@mock.patch('dependency_metrics.package_managers.pip.Pip')
+class GetTotalCountForPipTests(TestCase):
+
+    def test_returns_accurate_count(self, mock_pip):
+        mock_pip.list.return_value = '[{"name": "test", "version": "1.0", ' \
+                                     '"latest_filetype": "wheel"}, {"name": ' \
+                                     '"test2", "version": "5.1", ' \
+                                     '"latest_filetype": "wheel"}]'
+        result = get_total_count_for_pip()
+        self.assertEqual(result, 2)
